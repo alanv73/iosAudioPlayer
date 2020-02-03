bc8c14dae03d060abd19b948b64f7d034ae012f1
Alan Van Art <alanv73@gmail.com>
2020-02-03 13:40:29 -0500

    initial commit

--- audioplayer/ViewController.swift
+++ audioplayer/ViewController.swift
@@ -7,14 +7,49 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player = AVAudioPlayer()
+    var timer = Timer()
+    
+    @IBOutlet weak var scrubber: UISlider!
+    @IBOutlet weak var sliderVolume: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let audioPath = Bundle.main.path(forResource: "ffdp", ofType: "mp3")
+        
+        do {
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+            scrubber.maximumValue = Float(player.duration)
+        } catch {
+            
+        }
     }
 
-
+    @IBAction func playClicked(_ sender: Any) {
+        player.play()
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateScrubber), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func pauseClicked(_ sender: Any) {
+        player.pause()
+        timer.invalidate()
+    }
+    
+    @IBAction func sliderMoved(_ sender: Any) {
+        player.volume = sliderVolume.value
+    }
+    
+    @IBAction func scrubMoved(_ sender: Any) {
+        player.currentTime = TimeInterval(scrubber.value)
+    }
+    
+    @objc func updateScrubber(){
+        scrubber.value = Float(player.currentTime)
+    }
 }
 
