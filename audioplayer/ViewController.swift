1ce235d65bc2005744545f1fc8ca9c14a3d08a34
Alan Van Art <alanv73@gmail.com>
2020-02-04 13:50:57 -0500

    add new songs add queueing for songfest

--- audioplayer/ViewController.swift
+++ audioplayer/ViewController.swift
@@ -13,31 +13,31 @@ class ViewController: UIViewController {
     
     var player = AVAudioPlayer()
     var timer = Timer()
+    var songList = [Int: [String: String]]()
+    var currentSong = 0
     
     @IBOutlet weak var scrubber: UISlider!
     @IBOutlet weak var sliderVolume: UISlider!
+    @IBOutlet weak var lblScrubber: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let audioPath = Bundle.main.path(forResource: "ffdp", ofType: "mp3")
+        loadSongs()
         
-        do {
-            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
-            scrubber.maximumValue = Float(player.duration)
-        } catch {
-            
-        }
+        queueSong()
     }
 
     @IBAction func playClicked(_ sender: Any) {
-        player.play()
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateScrubber), userInfo: nil, repeats: true)
-    }
-    
-    @IBAction func pauseClicked(_ sender: Any) {
-        player.pause()
-        timer.invalidate()
+        if player.isPlaying {
+            player.pause()
+            timer.invalidate()
+        } else {
+            player.play()
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateScrubber), userInfo: nil, repeats: true)
+        }
+        
+        
     }
     
     @IBAction func sliderMoved(_ sender: Any) {
@@ -50,6 +50,72 @@ class ViewController: UIViewController {
     
     @objc func updateScrubber(){
         scrubber.value = Float(player.currentTime)
+    }
+    
+    @IBAction func skipBackClicked(_ sender: Any) {
+        var playing = false
+        if player.isPlaying {
+            playing = true
+        }
+
+        if currentSong > 0 {
+            currentSong -= 1
+            
+            queueSong()
+            
+            scrubber.value = 0
+            if playing {
+                player.play()
+            }
+        }
+    }
+    
+    @IBAction func skipForwardClicked(_ sender: Any) {
+        var playing = false
+        if player.isPlaying {
+            playing = true
+        }
+        
+        if currentSong < (songList.count - 1) {
+            currentSong += 1
+
+            queueSong()
+            
+            scrubber.value = 0
+            if playing {
+                player.play()
+            }
+            
+        }
+    }
+    
+    func loadSongs() {
+        var song = [String: String]()
+        
+        song["title"] = "Instant Karma"
+        song["artist"] = "John Lennon"
+        song["file"] = "JLIK"
+        songList[0] = song
+        
+        song["title"] = "Cecilia"
+        song["artist"] = "Simon & Garfunkel"
+        song["file"] = "SGC"
+        songList[1] = song
+        
+    }
+    
+    func queueSong() {
+         let audioPath = Bundle.main.path(forResource: songList[currentSong]!["file"], ofType: "mp3")
+        
+         do {
+             try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+             scrubber.maximumValue = Float(player.duration)
+            
+            lblScrubber.text = "\(String(describing: songList[currentSong]!["title"])) by \(String(describing: songList[currentSong]!["artist"]))"
+         } catch {
+             
+         }
+
     }
 }
 
